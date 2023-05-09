783d6937b945edb233b3acd513544fd76de258c7
RadukDumi <115774967+RadukDumi@users.noreply.github.com>
2023-05-09 14:55:11 +0300

    SQL C# con

---
+++ C# SQL con/BookDAO.cs
@@ -0,0 +1,40 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public class BookDAO
+    {
+        public static string GetBooks()
+        {
+            OracleConnection con = Database.GetConnection();
+
+            OracleCommand command;
+            OracleDataReader dataReader;
+            String sql, Output = "";
+
+            sql = "Select * from CARTI";
+            command = new OracleCommand(sql, con);
+            dataReader = command.ExecuteReader();
+            while (dataReader.Read())
+            {
+                Output = Output + dataReader.GetValue(0)
+                    + " - " + dataReader.GetValue(1)
+                    + " - " + dataReader.GetValue(2)
+                    + " - " + dataReader.GetValue(3)
+                    + " - " + dataReader.GetValue(4)
+                    + " - " + dataReader.GetValue(5) + "\n";
+            }
+
+            Database.ClosedConnection();
+
+            return Output;
+        }
+    }
+}
---
+++ C# SQL con/Database.cs
@@ -0,0 +1,50 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public static class Database
+    {
+        public static OracleConnection con = null;
+
+        private static void CreateConnection()
+        {
+            con = new OracleConnection();
+            // create connection string using builder
+            OracleConnectionStringBuilder ocsb = new OracleConnectionStringBuilder();
+            ocsb.Password = "STUDENT";
+            ocsb.UserID = "STUDENT";
+            ocsb.DataSource = "localhost:1521/XE";
+
+            // connect
+            con.ConnectionString = ocsb.ConnectionString;
+            con.Open();
+            MessageBox.Show("Connection Open!");
+        }
+
+        public static OracleConnection GetConnection()
+        {
+            if(con == null) 
+            {
+                CreateConnection();
+            }
+            return con;
+        }
+
+        public static void ClosedConnection()
+        {
+            con.Close();
+            MessageBox.Show("Connection Closed!");
+            con = null;
+        }
+    }
+}
